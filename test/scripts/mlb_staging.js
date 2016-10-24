d3b2879f05ed47a53f77180de6b8328a74b648a4
jso528 <jso528@gmail.com>
2016-10-24 07:40:18 -0700

    added missing comma

--- test/scripts/mlb_staging.js
+++ test/scripts/mlb_staging.js
@@ -4,7 +4,7 @@ var util = require('../../test/util');
 var startUrl = 'https://dodgers-staging.trumedianetworks.com:' + process.env.PORT_NUMBER;
 
 var tests = [
-  './mlb/login_page'
+  './mlb/login_page',
   './mlb/standings_page',
   './mlb/scores_page',
   './mlb/detailed_score_page',
